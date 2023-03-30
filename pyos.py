0c0975462c91540c349dac084eab9669beb9daf1
Yuri <97847334+yurigauze@users.noreply.github.com>
2023-03-29 21:33:55 -0300

    Add files via upload

--- pyos.py
+++ pyos.py
@@ -14,47 +14,54 @@ class os_t:
 
 		self.console_str = ""
 		self.terminal.console_print("this is the console, type the commands here\n")
-		print("AQUI")
+		self.keyboard_buffer = ""
 
 	def printk(self, msg):
 		self.terminal.kernel_print("kernel: " + msg + "\n")
-		print("AQUI")
 
 	def panic (self, msg):
 		self.terminal.end()
 		self.terminal.dprint("kernel panic: " + msg)
 		self.cpu.cpu_alive = False
 		#cpu.cpu_alive = False
-		print("AQUI")
 
 	def interrupt_keyboard (self):
 		key = self.terminal.get_key_buffer()
 
-		if key == curses.KEY_BACKSPACE:
-		    #Apaga o ultimo digito da string de console
-        		self.console_str = self.console_str[:-1]
-		    #Atualizar o console
-		    	self.terminal.console_print("\rDigite os comandos aqui: " + self.console_str + " ")
-		    	self.terminal.refresh()
-		elif (key == curses.KEY_ENTER):
-		    #Executar o comando digitado pelo usuario
-		    self.terminal.console_print("\n")
-		    self.execute_command()
-		    self.console_str = ""
-		elif ((key >= ord('a')) and (key <= ord('z'))) or ((key >= ord('A')) and (key <= ord('Z'))) or ((key >= ord('0')) and (key <= ord('9'))) or (key == ord(' ')) or (key == ord('-')) or (key == ord('_')) or (key == ord('.')):
-        	#Adicionar o caractere digitado a string de console
-		    self.console_str += chr(key)
-		    #Atualizar o console
-		    self.terminal.console_print(chr(key))
-		    self.terminal.refresh()
+		if ((key >= ord('a')) and (key <= ord('z'))) or ((key >= ord('A')) and (key <= ord('Z'))) or ((key >= ord('0')) and (key <= ord('9'))) or (key == ord(' ')) or (key == ord('-')) or (key == ord('_')) or (key == ord('.')):
+			self.console_str  += chr(key)
+			self.terminal.console_print("\r" + self.console_str)
+
+		elif key == curses.KEY_BACKSPACE:
+			self.console_str = self.console_str[:-1]
+			self.terminal.clear()
+			self.terminal.console_print(self.console_str)
+			return
+
+		elif (key == curses.KEY_ENTER) or (key == ord('\n')):
+			self.execute_command(self.console_str)
+			self.console_str  = ""
+
+
+
 
 	def handle_interrupt (self, interrupt):
-		# Verifica se a interrucaoca e do teclado (IRQ1)
-		print("sera que foi?")
-		if interrupt.type == 2:
+		if interrupt == 2:
+			self.printk("interrupcao recebida: %d" % interrupt)
 			self.interrupt_keyboard()
 		else:
-			return
+			self.printk("interrupcao recebida: %d" % interrupt)
+
+
+	def execute_command(self, command):
+		if command == "fechar":
+			os.system('clear')
+			os.kill(os.getpid(), 2)
+			os.system('clear')
+		elif command == "carregar":
+			self.printk("Carregado")
+		else:
+			self.printk("comando desconhecido: %s" % command)
 
 	def syscall (self):
 		#self.terminal.app_print(msg)
